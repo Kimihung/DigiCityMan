5174d1d5533abbbb8038e39ec852bcea9d030cff
Cinque <cinque.ck@hotmail.com>
2015-02-07 23:54:22 +0800

    0.14

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var map;
+  require(["esri/map", "dojo/domReady!"], function(Map) { 
+    map = new Map("map", {
+      center: [-56.049, 38.485],
+      zoom: 3,
+      basemap: "streets"
+    });
+});
+window.onready = function(){
+	
+};
